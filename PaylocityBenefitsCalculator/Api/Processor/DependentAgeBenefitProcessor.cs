ce886459f7ed14f49d49a22aef0bd85f0b5884a4
sheung1819 <sheung1020@outlook.com>
2024-01-27 08:44:38 -0800

    updated dependen age benefit

--- PaylocityBenefitsCalculator/Api/Processor/DependentAgeBenefitProcessor.cs
+++ PaylocityBenefitsCalculator/Api/Processor/DependentAgeBenefitProcessor.cs
@@ -4,11 +4,16 @@ namespace Api.Processor
 {
     public class DependentAgeBenefitProcessor : IBenefitProcessor
     {
+        private readonly decimal _benefitDependentAgeCost; 
+        public DependentAgeBenefitProcessor(IConfiguration configuration)
+        {
+            _benefitDependentAgeCost = configuration.GetValue<decimal>("Benefit:BenefitDependentAgeCost");
+        }
         public decimal CalculateBenefit(Employee employee)
         {
             var dependents = employee.Dependents.Where(x => Helper.CalculateAgeHelper.CalculateAge(x.DateOfBirth) > 50);
 
-            return dependents.Count() * 200;
+            return dependents.Count() * _benefitDependentAgeCost;
         }
     }
 }
