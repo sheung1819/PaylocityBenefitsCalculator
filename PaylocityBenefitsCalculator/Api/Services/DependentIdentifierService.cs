364b79c7af6d649de6e30fd987119d895a065ab3
sheung1819 <sheung1020@outlook.com>
2024-01-27 23:35:34 -0800

    updated code

---
+++ PaylocityBenefitsCalculator/Api/Services/DependentIdentifierService.cs
@@ -0,0 +1,24 @@
+﻿using Api.Models;
+
+namespace Api.Services
+{
+    public interface IDependentIdentifierService
+    { 
+        int GetDependentCount(Employee employee);
+    }
+    public class DependentIdentifierService : IDependentIdentifierService
+    {
+        private readonly IList<string> _dependentRelationship;
+        public DependentIdentifierService(IConfiguration configuration)
+        {
+            var relationship = configuration.GetValue<string>("QualifyDependentRelationType");
+
+            _dependentRelationship = relationship.Split(",");
+
+        }
+        public int GetDependentCount(Employee employee)
+        {
+            return employee.Dependents.Select(x => !_dependentRelationship.Contains(x.Relationship.ToString())).Count();
+        }
+    }
+}
