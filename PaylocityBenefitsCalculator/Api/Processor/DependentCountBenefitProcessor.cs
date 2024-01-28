655098c04a25c890ddefdd2290c6756d171ccb3f
sheung1819 <sheung1020@outlook.com>
2024-01-28 12:32:26 -0800

    update service

--- PaylocityBenefitsCalculator/Api/Processor/DependentCountBenefitProcessor.cs
+++ PaylocityBenefitsCalculator/Api/Processor/DependentCountBenefitProcessor.cs
@@ -1,12 +1,22 @@
 ﻿using Api.Models;
+using Api.Services;
 
 namespace Api.Processor
 {
     public class DependentCountBenefitProcessor : IBenefitProcessor
     {
+        private readonly decimal _dependentCost;
+        private readonly IDependentQualifyService _qualifyService;
+        public DependentCountBenefitProcessor(IConfiguration configuration, DependentQualifyService dependentQualifyService)         
+        {
+            _dependentCost = configuration.GetValue<decimal>("Benefit:DependentCost");
+            _qualifyService = dependentQualifyService;
+        } 
         public decimal CalculateBenefit(Employee employee)
         {
-            return employee.Dependents.Count() * 600; 
+            var dependentCount = _qualifyService.GetDependentCount(employee);
+
+            return dependentCount * _dependentCost; 
         }
     }
 }
