e63f1bf250ebd41b017a192a08cb80eba0851976
sheung1819 <sheung1020@outlook.com>
2024-01-28 21:11:50 -0800

    UPDATE

---
+++ PaylocityBenefitsCalculator/ApiTests/UnitTests/PaycheckserviceTests.cs
@@ -0,0 +1,36 @@
+﻿using Api.Processor;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ApiTests.UnitTests
+{
+    public class PaycheckServiceTests
+    {
+        private Mock<BaseCostBenefitProcessor> _mockBaseCost;
+        private List<IBenefitProcessor> _processors;
+        public PaycheckServiceTests() 
+        {
+            _mockBaseCost = (new Mock<BaseCostBenefitProcessor>());
+
+            _processors.Add(_mockBaseCost.Object);
+
+        
+        }
+
+
+        [Fact]
+        public void PaycheckService_Should_Calculate_Monthly_Paycheck()
+        { 
+
+            
+        }
+
+        
+        
+    }
+}
