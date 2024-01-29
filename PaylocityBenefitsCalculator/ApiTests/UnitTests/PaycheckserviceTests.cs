a91b807377e8925c611e56c1739e588ce9926df5
sheung1819 <sheung1020@outlook.com>
2024-01-28 23:14:27 -0800

    updated test

--- PaylocityBenefitsCalculator/ApiTests/UnitTests/PaycheckserviceTests.cs
+++ PaylocityBenefitsCalculator/ApiTests/UnitTests/PaycheckserviceTests.cs
@@ -1,10 +1,10 @@
-﻿using Api.Processor;
+﻿using Api.Models;
+using Api.Processor;
+using Api.Repositories;
+using Api.Services;
+using AutoMapper;
 using Moq;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace ApiTests.UnitTests
@@ -12,12 +12,12 @@ namespace ApiTests.UnitTests
     public class PaycheckServiceTests
     {
         private Mock<BaseCostBenefitProcessor> _mockBaseCost;
-        private List<IBenefitProcessor> _processors;
+        private List<IBenefitProcessor> _processors = new List<IBenefitProcessor>();
         public PaycheckServiceTests() 
         {
-            _mockBaseCost = (new Mock<BaseCostBenefitProcessor>());
+            _mockBaseCost = new Mock<BaseCostBenefitProcessor>(ConfigurationHelper.Configuration);
 
-            _processors.Add(_mockBaseCost.Object);
+            //_processors.Add(_mockBaseCost.Object);
 
         
         }
@@ -26,8 +26,26 @@ namespace ApiTests.UnitTests
         [Fact]
         public void PaycheckService_Should_Calculate_Monthly_Paycheck()
         { 
+            var mockEmployeeRepository = new Mock<IEmployeeRepository>();
+            mockEmployeeRepository
+                .Setup(x => x.GetEmployeeByID(It.IsAny<int>()))
+                .Returns(new Api.Models.Employee());
 
-            
+            var mockBenefitService = new Mock<IBenefitService>();
+            mockBenefitService
+                .Setup(x => x.Calculate(It.IsAny<Employee>()))
+                .Returns(1000);
+
+            var mockMonthlyPaycheckCalculator = new Mock<IMonthlyPaycheckCalculator>();
+            mockMonthlyPaycheckCalculator
+                .Setup(x => x.Calculate(It.IsAny<Paycheck>()));
+
+            var mockMapper = new Mock<IMapper>();
+
+            var paycheckService = new PaycheckService(mockMapper.Object, mockEmployeeRepository.Object, mockBenefitService.Object, mockMonthlyPaycheckCalculator.Object);
+            var result = paycheckService.CalculateMonthlyPaycheck(11);
+            Assert.NotNull(result);
+
         }
 
         
